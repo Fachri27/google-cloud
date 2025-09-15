1b8d756f77d02099f850c53ab1a33f941a28319c
Fachri <mhmdfachri345@gmail.com>
2025-09-15 12:36:25 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        https: true, // biar dev server pakai https
+    }
 })
