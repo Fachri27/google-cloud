58a76ebe91bdb4489ae422c8183c9d8e68015c6f
Fachri <mhmdfachri345@gmail.com>
2025-09-15 12:50:01 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    base: '/build/',
+    base: process.env.ASSET_URL ? process.env.ASSET_URL + '/build/' : '/build/',
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
